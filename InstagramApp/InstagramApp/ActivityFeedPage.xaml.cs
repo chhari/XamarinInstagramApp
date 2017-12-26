39408fd551e770fbf26d34dbbee0c5fb4e811e4c
harisaran <harisaranch@gmail.com>
2017-12-26 00:51:27 -0500

    1st commit

---
+++ InstagramApp/InstagramApp/ActivityFeedPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿
+using Xamarin.Forms;
+
+namespace InstagramApp
+{
+	public partial class ActivityFeedPage : ContentPage
+	{
+		private ActivityService _service = new ActivityService();
+
+		public ActivityFeedPage()
+		{
+			InitializeComponent();
+
+		}
+
+		void OnActivitySelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+		{
+			if (e.SelectedItem == null)
+				return;
+
+			var activity = e.SelectedItem as Activity;
+
+			Navigation.PushAsync(new UserProfilePage(activity.UserId));
+		}
+	}
+}
